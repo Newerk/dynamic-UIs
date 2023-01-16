8880d857190ab20830b512dffbac53a4dfbee0f4
newerk <juniusowens@yahoo.com>
2023-01-16 16:28:24 -0500

    Dropdown with animation
    There is one issue where the textcontent of the clicked element doesnt change on the first click

--- drop-down-menu/dropdown.js
+++ drop-down-menu/dropdown.js
@@ -1,15 +1,44 @@
 const wrapper = document.querySelector('.wrapper');
 const main = document.querySelector('.main');
 
+let currentDropDownText = 'Home';
+
 main.addEventListener('click', () => {
     let options = document.querySelector('.options');
+    let choices = document.querySelectorAll('.choices');
+
 
     if (options.style.visibility === "visible") {
-        options.setAttribute('style', 'visibility: hidden');
+        options.setAttribute('style', 'visibility: hidden;');
+        textController(choices, options);
 
     } else {
-        options.setAttribute('style', 'visibility: visible');
+        options.setAttribute('style', 'visibility: visible;');
+        textController(choices, options);
 
     }
 })
 
+function textController(element, visibilityValue) {
+    element.forEach(el => {
+        if (visibilityValue.style.visibility === 'visible') {
+            el.setAttribute('style', 'height: 3rem; font-size: 1rem')
+        } else {
+            el.setAttribute('style', 'height: 0');
+        }
+
+        el.addEventListener('click', function (e) {
+            const clickedOption = e.currentTarget.textContent;
+            el.textContent = currentDropDownText;
+            currentDropDownText = main.textContent = clickedOption;
+
+
+            element.forEach(node => {
+                node.setAttribute('style', 'visibility: hidden;');
+
+            })
+            visibilityValue.style.visibility = 'hidden';
+        });
+    })
+
+}
