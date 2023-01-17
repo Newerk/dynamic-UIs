bf478d7c45b63fddc3e10f510669a5fb27ee290b
newerk <juniusowens@yahoo.com>
2023-01-17 10:47:20 -0500

    Refactor and Fix Dropdown Menu committed
    Dropdown menu is now fully functional with no error. Code has also been
    significantly shortened

--- drop-down-menu/dropdown.js
+++ drop-down-menu/dropdown.js
@@ -1,40 +1,28 @@
-const wrapper = document.querySelector('.wrapper');
 const main = document.querySelector('.main');
-
-let currentDropDownText = 'Home';
+let choices = document.querySelectorAll('.choices');
+let currentPage = main.textContent;
 
 main.addEventListener('click', () => {
-    let options = document.querySelector('.options');
-    let choices = document.querySelectorAll('.choices');
-
-
-    if (options.style.visibility === "visible") {
-        options.setAttribute('style', 'visibility: hidden;');
-        textController(choices, options);
-
+    if (document.querySelector('.choices').style.visibility === 'visible') {
+        closeMenu()
     } else {
-        options.setAttribute('style', 'visibility: visible;');
-        textController(choices, options);
 
+        choices.forEach(element => {
+            element.setAttribute('style', 'visibility: visible; height: 3rem; font-size: 1rem;')
+            element.addEventListener('click', swapText);
+        });
     }
 })
 
-function textController(element, visibilityValue) {
-    element.forEach(el => {
+function closeMenu() {
+    choices.forEach(element => {
+        element.setAttribute('style', 'visibility: hidden; height: 0; font-size: 0;');
+    });
+}
 
-        if (visibilityValue.style.visibility === 'visible') {
-            el.setAttribute('style', 'height: 3rem; font-size: 1rem')
-        } else {
-            el.setAttribute('style', 'height: 0');
-        }
-
-        el.addEventListener('click', function (e) {
-            let clickedOption = e.currentTarget.textContent;
-            main.textContent = currentDropDownText = el.textContent;
-            visibilityValue.style.visibility = 'hidden';
-            element.forEach(node => node.setAttribute('style', 'height: 0; font-size: 0'))
-        });
-
-    })
-
+function swapText(event) {
+    let clickedPage = event.target.textContent;
+    event.target.textContent = main.textContent;
+    main.textContent = clickedPage;
+    closeMenu();
 }
