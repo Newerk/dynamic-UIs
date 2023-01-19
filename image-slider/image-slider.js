e319c7750908f01ddb51810187c0d5b98252fb68
newerk <juniusowens@yahoo.com>
2023-01-19 13:43:56 -0500

    Change Size of Nav Dots Based on The Focused Img

--- image-slider/image-slider.js
+++ image-slider/image-slider.js
@@ -9,15 +9,16 @@ const buildDots = ((node) => {
     for (let i = 0; i < node.length; i++) {
         const dot = document.createElement('div');
         dot.className = 'dot';
+        dot.id = `pg${i + 1}`;
 
         dotsContainer.appendChild(dot)
     }
+
+    dotIndicator()
 })(scrollWrapper);
 
 leftBtn.addEventListener('click', goLeft)
 rightBtn.addEventListener('click', goRight)
-
-
 
 function goLeft() {
     if (start === 10) {
@@ -27,13 +28,10 @@ function goLeft() {
 
     if (start < 10) {
         document.getElementById('img1').style.marginLeft = `${start + 84}vw`;
-
         start += 84;
-
     }
-
+    dotIndicator()
     console.log(start)
-
 
 }
 
@@ -46,11 +44,42 @@ function goRight() {
 
     if (start > -326) {
         document.getElementById('img1').style.marginLeft = `${start - 84}vw`;
-
         start -= 84;
     }
-
-    console.log(start)
-
+    dotIndicator()
 }
 
+function dotIndicator() {
+
+    document.querySelector('.dots-container').childNodes.forEach(el => {
+       el.setAttribute('style', 'height: 1vh; width: 1vh;')
+    }
+    )
+
+    switch (start) {
+        case 10:
+            document.getElementById('pg1').setAttribute('style', 'height: 2vh; width: 2vh;')
+
+            break;
+        case -74:
+            document.getElementById('pg2').setAttribute('style', 'height: 2vh; width: 2vh;')
+
+            break;
+
+        case -158:
+            document.getElementById('pg3').setAttribute('style', 'height: 2vh; width: 2vh;')
+
+            break;
+
+        case -242:
+            document.getElementById('pg4').setAttribute('style', 'height: 2vh; width: 2vh;')
+
+            break;
+
+        case -326:
+            document.getElementById('pg5').setAttribute('style', 'height: 2vh; width: 2vh;')
+
+            break;
+    }
+
+}
